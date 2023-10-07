0fde4ffa0492866efbbca6d4d4bc758af3182063
monxchrome <inacheat@gmail.com>
2023-10-07 11:00:41 +0200

    str operators

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,42 +1,19 @@
-# from math import pow
-# import random
-# from statistics import mean
+# def test(a, b):                  // string
+#     print(f"{a} {b}")
 
-# a = 11
-# b = 14
+# test(input(), input())
 
-# c = a + b
-# d = a * b
-# e = a - b
-# f = a / b
+a = "hello world &"                  # str operators
 
-# print(c, d, e, f)
-
-# def function(a, b):
-#     print(a + b, a * b, a - b, a / b, a % b, sep="\n")
-
-# function(11, 14)
-
-# def test (a, b):
-#     print(a*b, (a+b)*2, sep="\n")
-
-# test(4, 5)
-
-# def test(a):
-#     print(pow(a, 2))
-
-# test(2)
-
-# def test(d, s):
-#     print(((d+s)*2)*4)
-
-# test(2, 4)
-
-# arr = [random.randint(5, 10), random.randint(5, 10), random.randint(5, 10), random.randint(5, 10), random.randint(5, 10)]
-
-# print(mean(arr))
-
-# def test(p, q):
-#     print((4*p + 4*q) * 5)
-
-# test(2, 5)
+print(
+    a.upper(), 
+    a.lower(),
+    a.strip("&"), 
+    a.split(),
+    a.find("w"), 
+    a.startswith("o"), 
+    a.startswith("h"),
+    a.endswith("a"), 
+    a.endswith("d"),
+    sep="\n"
+    )
