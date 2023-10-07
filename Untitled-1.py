26df98defad57c8ba5166be5de69644840c4c942
monxchrome <inacheat@gmail.com>
2023-10-07 11:08:59 +0200

    f-string

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,19 +1,31 @@
-# def test(a, b):                  // string
+# string
+
+# def test(a, b):                 
 #     print(f"{a} {b}")
 
 # test(input(), input())
 
-a = "hello world &"                  # str operators
+# str operators
 
-print(
-    a.upper(), 
-    a.lower(),
-    a.strip("&"), 
-    a.split(),
-    a.find("w"), 
-    a.startswith("o"), 
-    a.startswith("h"),
-    a.endswith("a"), 
-    a.endswith("d"),
-    sep="\n"
-    )
+# a = "hello world &"                 
+
+# print(
+#     a.upper(), 
+#     a.lower(),
+#     a.strip("&"), 
+#     a.split(","),
+#     a.split(),
+#     a.find("w"), 
+#     a.startswith("o"), 
+#     a.startswith("h"),
+#     a.endswith("a"), 
+#     a.endswith("d"),
+#     sep="\n"
+#     )
+
+# f string (magic string)
+
+def fstr(a, b):
+    print(f"Hello my name is {a} I'm {b} years old")
+
+fstr("Stefan", 18)
