de6371c1eb55208a20c179576b4a6d97a57a5a9d
monxchrome <inacheat@gmail.com>
2023-10-20 17:30:06 +0200

    all tasks

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,5 @@
 from __future__ import print_function, unicode_literals
-from InquirerPy import prompt
+from InquirerPy import prompt, inquirer
 
 def task1(a,b):
     elements = [item for item in a if item in b]
@@ -190,3 +190,156 @@ def task9():
             break
 
 task9()
+
+def task10():
+    books = [
+            {"name": "transcedence", "price": 10},
+            {"name": "anabel", "price": 15},
+            {"name": "harry potter", "price": 25},
+            {"name": "NFS", "price": 20},
+        ]
+    
+    sections = [
+        {"name": "All books", "value": "all_books"},
+        {"name": "Add book", "value": "add_book"},
+        {"name": "Update book", "value": "update_book"}
+    ]
+
+    selected_choiсe = inquirer.select(
+        message="Choose books:",
+        choices = sections
+    ).execute()
+
+    if selected_choiсe == "all_books":
+        print("All books:")
+        for book in books:
+            print(f"{book['name']} - {book['price']}")
+    elif selected_choiсe == "add_book":
+        add_book = inquirer.text(
+            message="Add the name of book"
+        ).execute()
+
+        add_price = inquirer.text(
+            message="Add price of the book"
+        ).execute()
+
+        add_price = float(add_price)
+
+        new_book = {"name": add_book, "price": add_price}
+        books.append(new_book)
+
+        print(f"The new book {add_book} - {add_price} has been added")
+    elif selected_choiсe == "update_book":
+        book_name = [book['name'] for book in books]
+
+        selected_book = inquirer.select(
+            message="Choose a book to change the price:",
+            choices=book_name
+        ).execute()
+
+        for book in books:
+            if book['name'] == selected_book:
+                new_price = inquirer.text(
+                    message = "Enter the value to change price of the book"
+                ).execute()
+
+                new_price = float(new_price)
+
+                book["price"] = new_price
+
+                print(f"The price of {book['name']} has been changed to {new_price}")
+                break
+        else:
+            print("Error")
+    else:
+        print("Error")
+
+task10()
+
+def task11():
+    print("Hello, user, welcome to the phone number dictionary!")
+name_contact = {}
+
+def add_contact():
+    name = input("Type the name: ")
+    number = input("number: ")
+    name_contact[name] = number
+    print(f"Contact {name} added!")
+
+def search_contact():
+    name = input("Type whom you want to find: ")
+    if name in name_contact:
+        print(f"The phone number for {name} is - {name_contact[name]}")
+    else:
+        print(f"{name} is not in the book")
+
+def change_number():
+    name = input("Type who's number to change: ")
+    if name in name_contact:
+        new_number = input("Type new number: ")
+        name_contact[name] = new_number
+    else:
+        print("There is no person with that name in book!")
+
+
+def delete_contact():
+    name = input("Type whom to delete: ")
+    if name in name_contact:
+        del name_contact[name]
+    else:
+        print("not in the book")
+
+
+while True:
+    print("Here are the options: \n 1) Add new contact. \n 2)search for a number by name. \n 3)Change the number of a contact. \n 4)Delete the contact. \n 5) exit ")
+    choice = input()
+    
+    if choice == "1":
+        print(name_contact)
+        add_contact()
+    elif choice == "2":
+        print(name_contact)
+        search_contact()
+    elif choice == "3":
+        print(name_contact)
+        change_number()
+    elif choice == "4":
+        print(name_contact)
+        delete_contact()
+    elif choice == "5":
+        print("Exiting phonebook. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
+task11()
+
+def task12(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return task12(n - 1) + task12(n - 2)
+print(task12(int(input())))
+
+def task13(n):
+    if n < 10:
+        return 1
+    
+    return 1 + task13(n//10)
+
+print(task13(int(input())))
+
+def task14(s):
+    s = s.lower()
+    s = ''.join(filter(str.isalnum, s))
+
+    if len(s) <= 1:
+        return True
+    elif s[0] != s[-1]:
+        return False
+    else:
+        return task14(s[1:-1])
+    
+print(task14("A man, a plan, a canal, Panama"))
